2a53edb45009362e4a105037e3d0891a7efd5557
Jamie <james.dalton@quickrelease.co.uk>
2022-06-16 15:24:35 +0100

    f-strings added, removed RAW BOM import

--- ETRSUpdate.py
+++ ETRSUpdate.py
@@ -1,12 +1,13 @@
 import gspread
 from datetime import date, timedelta
+import datetime
 import pandas as pd
 import openpyxl
 import os
 import glob
 
 global basePath
-basePath = r"S:\PDM Files\P1 - Mustang\\"
+basePath = "S:\PDM Files\P1 - Mustang\\"
 global targetPath
 targetPath = fr"{basePath}\ETRS\ETRS Master\ETRS v3 Master.xlsx"
 
@@ -36,15 +37,18 @@ def write_to_finance_update_csv():
 
 def load_data_file(sourcePath):
     # load data file using pandas
-    if sourcePath[-3:] == "csv":
+    if sourcePath[-4:] == "csv ":
         print("CSV found, reading csv")
         data = pd.read_csv(sourcePath)
-    elif sourcePath[10:] == "BOM Export":
+    elif sourcePath[-24:-14] == "BOM Export":
         print("BOM Export found, reading excel")
         data = pd.read_excel(sourcePath, sheet_name="Formatted BOM")
     elif sourcePath[-4:] == "xlsx":
         print("Excel doc found, reading excel")
         data = pd.read_excel(sourcePath)
+    else:
+        print("Document not found")
+
     return data
 
 
@@ -52,17 +56,20 @@ def write_to_ETRS():
     # Gen's feedback - f(strings), create a base path variable,
     BOMExportPath = "\BOM\BOM Exports\BOM Export "
     today = date.today()
+    monday = today - datetime.timedelta(days=today.weekday())
+    mondayBOMFormat = str(monday.strftime('%Y%m%d'))
     todayBOMFormat = str(today.strftime('%Y%m%d'))
-    yesterdayBOMFormat = str(today - timedelta(days=1).strftime('%Y%m%d'))
-    weekendBOMFormat = str(today - timedelta(days=3).strftime('%Y%m%d'))
+    yesterdayBOMFormat = str((today - timedelta(days=1)).strftime('%Y%m%d'))
+    weekendBOMFormat = str((today - timedelta(days=3)).strftime('%Y%m%d'))
 
     existingFileList = glob.glob(f"{basePath}\ETRS\*.xlsx")
-    financeSource = f"{basePath}\ETRS\DataFiles\\Finance {today}.csv "
-    todayBOMSource = f"{basePath}{BOMExportPath}{todayBOMFormat}.xlsx"
-    yesterdayBOMSource = f"{basePath}{BOMExportPath}{yesterdayBOMFormat}.xlsx"
-    weekendBOMSource = f"{basePath}{BOMExportPath}{weekendBOMFormat}.xlsx"
+    financeSource = fr"{basePath}ETRS\DataFiles\Finance {today}.csv "
+    todayBOMSource = fr"{basePath}{BOMExportPath}{todayBOMFormat}.xlsx"
+    yesterdayBOMSource = fr"{basePath}{BOMExportPath}{yesterdayBOMFormat}.xlsx"
+    weekendBOMSource = fr"{basePath}{BOMExportPath}{weekendBOMFormat}.xlsx"
+    mondayBOMSource = fr"{basePath}{BOMExportPath}{mondayBOMFormat}.xlsx"
     workflowSource = fr"{basePath}\BOM\Upchain Custom Reports\EBOM Reports\eBOM Workflow Report {todayBOMFormat}.xlsx"
-    savePath = f"{basePath}\ETRS\ETRS Exports\\ETRS " + str(date.today())
+    savePath = fr"{basePath}\ETRS\ETRS Exports\\ETRS " + str(date.today())
 
     print("Loading ETRS Workbook " + targetPath)
     book = openpyxl.load_workbook(targetPath)
@@ -83,6 +90,8 @@ def write_to_ETRS():
             except:
                 print("ERROR Yesterday's BOM and last Friday's BOM not found.")
                 # Add a user input date picker here?
+        print("Loading Monday's BOM Export Data")
+        mondayBOMData = load_data_file(mondayBOMSource)
 
         print("Loading Workflow Data")
         workflowData = load_data_file(workflowSource)
@@ -91,42 +100,44 @@ def write_to_ETRS():
         sheetName1 = "BOM Export"
         sheetName2 = "Purchasing Lead Times"
         sheetName3 = "Workflow"
-        sheetName4 = "Raw BOM"
-        sheetName5 = "Old BOM Export"
+        sheetName5 = "Yesterday BOM Export"
+        sheetName6 = "Monday's BOM Export"
         writer.book = book
         writer.sheets = {ws.title: ws for ws in book.worksheets}
 
         print("Writing Finance Data to ETRS")
         financeData.to_excel(writer, sheet_name=sheetName2)
         print("Writing today's BOM Export Data to ETRS")
-        todayBOMData.to_excel(writer, sheet_name=sheetName1)
+        todayBOMData.to_excel(writer,sheet_name=sheetName1)
         print("Writing yesterday's BOM Export Data to ETRS")
-        todayBOMData.to_excel(writer, sheet_name=sheetName5)
-        print("Writing Raw BOM Export Data to ETRS")
-        yesterdayBOMData.to_excel(writer, sheet_name=sheetName4)
+        yesterdayBOMData.to_excel(writer,sheet_name=sheetName5)
+        print("Writing Monday's BOM Export Data to ETRS")
+        mondayBOMData.to_excel(writer, sheet_name=sheetName6)
         print("Writing Workflow Export Data to ETRS")
-        workflowData.to_excel(writer, sheet_name=sheetName3)
+        workflowData.to_excel(writer,sheet_name=sheetName3)
+
+
         print("Saving Master...")
         book.save(f"{basePath}\ETRS\ETRS Master\ETRS v3 Master.xlsx")
         print("Master Saved!")
         print("Saving Export...")
 
         # Archive existing .xlsx files in the main directory.
-        for i in existingFileList:
-            archiveFilename = f"{basePath}\ETRS\Archive\\" + i[31:]
-            try:
-                os.rename(i, archiveFilename)
-            except:
-                pass
+        # for i in existingFileList:
+        #     archiveFilename = f"{basePath}\ETRS\Archive\\" + i[31:]
+        #     try:
+        #         os.rename(i, archiveFilename)
+        #     except:
+        #         pass
 
-        book.save(f"{basePath}\ETRS\\ETRS " + str(date.today()) + ".xlsx")
+        book.save(f"{basePath}\ETRS\ETRS Master\\" + str(date.today()) + " JD TEST.xlsx")
         print("Export Saved!")
 
 
 def main():
     print("Updating ETRS...")
-    # write_to_finance_update_csv()
-    # write_to_ETRS()
+    write_to_finance_update_csv()
+    write_to_ETRS()
     print("Update Complete")
 
 if __name__ == "__main__":
